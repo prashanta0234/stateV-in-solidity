96e5e9c902698bb06d9134f154b9f96b2b85b572
prashnata chakrabotyr <prashanta0234@gmail.com>
2022-03-05 04:16:44 +0600

    first commit

---
+++ state.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract man {
+    uint256 public age;
+    string public name;
+
+    function setMan() public {
+        age = 20;
+        name = "Shanto";
+    }
+}
